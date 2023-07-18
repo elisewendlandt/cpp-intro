05405f12bc8f19342ccd65ec6e432cab2d138241
elise <elisewendlandt@gmail.com>
2023-07-18 14:37:26 -0400

    Main.cpp making variables

--- main.cpp
+++ main.cpp
@@ -2,11 +2,20 @@
 
 int main()
 {
-  std::cout << "Hello, world!" << std::endl;
+  std::cout << "Elise Wendlandt, 115" << std::endl;
 
   int x = 5;
   int y = 7;
-  std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
+  int z = x + y;
+  std::cout << "The sum of " << x << " and " << y << " is " << x+y<< std::endl;
+  std::cout << z<< std::endl;
+
+  int w = x * y * z;
+  std::cout << "The product of " << x << " and " << y << "and " << z << " is " << w << std::endl;
+  std::cout << w << std::endl;
+  int v = x/y;
+  std::cout << "The quotient of " << x << " and " << y << " is " << v << std::endl;
+  std::cout << "This is " << v << std::endl;
 
   return 0;
 }
